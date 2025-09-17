f526a82e4e9b0d048802ba479254272bb21109c0
Adinette <108472474+Adinette@users.noreply.github.com>
2025-09-17 12:08:26 +0100

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,32 +6,6 @@
 
 
 export const projects = [
-
-
-	// 	{
-	// 	"name": "",
-	// 	"description": "Vente et location de matériaux de constructions.",
-	// 	"img": "/assets/images/projects/bati-vie.jpeg",
-	// 	"url": "https://bati-vie-tfw7.vercel.app/home "
-	// },
-	// {
-	// 	"name": "Colloc Services",
-	// 	"description": "Vente et réservation de voitures.",
-	// 	"img": "/assets/images/projects/colloc.jpeg",
-	// 	"url": "https://colloc.vercel.app"
-	// },
-	// {
-	// 	"name": "Funiro",
-	// 	"description": "Un site qui permet de donner de la visibilité pour vendre des meubles.",
-	// 	"img": "/assets/images/projects/funiro.jpeg",
-	// 	"url": "https://funiro-phi.vercel.app/"
-	// },
-	// {
-	// 	"name": "Food",
-	// 	"description": "Un site de commande de repas / rapide / sécurisé.",
-	// 	"img": "/assets/images/projects/food.jpeg",
-	// 	"url": "https://food-gilt-xi.vercel.app"
-	// }
 	{
 		title: "TodoList App",
 		techStack: "Vuejs • TypeScript • NodeJs • Tailwind",
@@ -53,7 +27,6 @@ export const projects = [
 		ctaText: "Live Demo →",
 		ctaLink: "#",
 	},
-
 	{
 		title: "Funiro",
 		techStack: "Html • Css • JavaScript • Bootstrap • Figma",
@@ -64,7 +37,7 @@ export const projects = [
 	{
 		title: "Food",
 		techStack: "Html • Css • JavaScript • Bootstrap • Figma",
-		description: "A fast and secure meal ordering website.
+		description: "A fast and secure meal ordering website".
 		ctaText: "Live Demo →",
 		ctaLink: "#",
 	},
